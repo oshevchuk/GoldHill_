f10c0f0aacfe34cf4d61a37082cfc039941f7e46
megadevice83 <megadevice83@gmail.com>
2018-01-03 14:45:09 +0200

    'fixes'

--- js/main.js
+++ js/main.js
@@ -45,3 +45,31 @@ previous.onclick = function () {
     // pauseSlideshow();
     previousSlide();
 };
+
+
+//on scroll animation
+var $animatedElements=$('.animated');
+var $window=$(window);
+$window.on('scroll', check_view);
+
+function check_view() {
+    var window_height=$window.height();
+    var window_top_position=$window.scrollTop();
+    var window_bottom_position=(window_top_position+window_height);
+
+    $.each($animatedElements, function () {
+        var $element=$(this);
+        var element_height=$element.outerHeight();
+        var element_top_position=$element.offset().top;
+        var element_bottom_position=(element_top_position+element_height);
+
+        if((element_bottom_position>=window_top_position) && (element_top_position<=window_bottom_position)){
+            $element.addClass('show');
+        }else{
+            // $element.removeClass('show');
+        }
+    })
+}
+
+$window.on('scroll resize');
+$window.trigger('scroll');
