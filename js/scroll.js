fba3741efcd25179285a0dd8557b6b27e3efc1a4
oshevchuk <megadevice83@gmail.com>
2018-01-09 19:52:47 +0200

    animations

---
+++ js/scroll.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Matrix on 09.01.2018.
+ */
+var $animatedElements=$('.animated');
+var $window=$(window);
+$window.on('scroll', check_view);
+
+function check_view() {
+    var window_height=$window.height();
+    var window_top_position=$window.scrollTop();
+    var window_bottom_position=(window_top_position+window_height);
+
+    $.each($animatedElements, function () {
+        var delta=100;
+        var $element=$(this);
+        var element_height=$element.outerHeight();
+        var element_top_position=$element.offset().top;
+        var element_bottom_position=(element_top_position+element_height);
+
+        if((element_bottom_position>=window_top_position) && (element_top_position<=window_bottom_position)){
+            $element.addClass('show');
+        }else{
+            // $element.removeClass('show');
+        }
+    })
+}
+
+$window.on('scroll resize');
+$window.trigger('scroll');
